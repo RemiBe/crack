9688e58c9f68b2b967538e1d8d32d9b1f359a41c
Remi Barat <remibarat@yahoo.fr>
2018-06-16 13:58:04 +0200

    utils # name dir/files

---
+++ utils/names.py
@@ -0,0 +1,35 @@
+"""Functions to name repositories/files, for example depending on the
+current time.
+"""
+
+__version__ = "1.0"
+__author__ = "Remi Barat"
+
+
+import datetime   as dt
+
+
+def get_time():
+    """Returns the date in a string of the form: `yyyy_mm_dd-hh_mm'.
+
+    Exemple :
+        `2015_03_17-14_04' is the kind of output.
+    """
+    return dt.datetime.now().strftime("%Y_%m_%d-%H_%M")
+
+
+def get_basename(path, extension):
+    """Returns the filename (without its whole path) without its
+    extension.
+
+    Argument:
+        path: str
+        extension: str: The extension (with a dot if there is one).
+
+    Example:
+        >>> print get_basename("~/Meshes/my_mesh.mesh", ".mesh")
+        ... my_mesh
+    """
+    return path.split("/")[-1][:-len(extension)]
+
+
